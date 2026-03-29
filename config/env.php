2a4c42133b6a80cc019d4af03ca807b46d13a4c5
KawaCoder <67145585+KawaCoder@users.noreply.github.com>
2026-03-29 18:54:27 +0200

    ajout infos db dans config

--- config/env.php
+++ config/env.php
@@ -3,11 +3,11 @@
 // Variables d'environnement pour l'API d'Authentification
 
 // -- BASE DE DONNÉES AUTH (AlwaysData) --
-define('DB_HOST_AUTH', '127.0.0.1');
-define('DB_NAME_AUTH', 'PHP_Rugby_Auth');
-define('DB_USER_AUTH', 'root');
-define('DB_PASSWORD_AUTH', '');
+define('DB_HOST_AUTH', 'mysql-authhh.alwaysdata.net');
+define('DB_NAME_AUTH', 'authhh');
+define('DB_USER_AUTH', 'authhh');
+define('DB_PASSWORD_AUTH', '!UBq69!G2rel');
 
 // Clé secrète pour générer et déchiffrer les Jetons de connexion
-define('JWT_SECRET', 'MON_SUPER_SECRET_POUR_LE_PROJET_R401_WxyZ123!_Rugby');
+define('JWT_SECRET', 'M_PIQUE_EST_TRES_BEAU');
 ?>
